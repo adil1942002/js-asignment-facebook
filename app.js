74344bba59116589ace8377dd3ad6933eaafc9d3
adil abid <117192462+adil1942002@users.noreply.github.com>
2023-05-24 04:52:09 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+var FirstName = document.getElementById("inp1")
+var Surname = document.getElementById("inp2")
+var Email = document.getElementById("inp3")
+var Password = document.getElementById("inp4")
+var birth = document.getElementById("birth")
+var months = document.getElementById("months")
+var years = document.getElementById("years")
+
+
+function Submit(){
+    console.log(FirstName.value);
+    console.log(Surname.value);
+    console.log(Email.value);
+    console.log(Password.value);
+    console.log(birth.value);
+    console.log(months.value);
+    console.log(years.value);
+
+
+}
